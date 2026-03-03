3d965cce1509c11cb3c170984303b46625b893d4
maksim.samorukov <maxim.samorukov@gmail.com>
2026-03-04 00:12:30 +0300

    fix styles

--- src/components/Shared/Background/index.tsx
+++ src/components/Shared/Background/index.tsx
@@ -3,10 +3,15 @@ import s from "./style.module.scss";
 export const Background = () => {
   const ref = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
+    if (ref.current) {
+      ref.current.style.setProperty("--mouse-x", `55%`);
+      ref.current.style.setProperty("--mouse-y", `40%`);
+    }
     const handleMouseMove = (e: MouseEvent) => {
       if (ref.current) {
         const x = e.clientX;
         const y = e.clientY;
+        console.log(x, y);
         ref.current.style.setProperty("--mouse-x", `${x}px`);
         ref.current.style.setProperty("--mouse-y", `${y}px`);
       }
