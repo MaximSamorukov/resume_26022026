7e6f2a754f37db274beed9d1ad59248d5806af75
maksim.samorukov <maxim.samorukov@gmail.com>
2026-03-05 17:15:31 +0300

    feat: add menu icons

--- src/components/Menu/MenuItem/index.tsx
+++ src/components/Menu/MenuItem/index.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import cn from "classnames";
+import AboutIcon from "@/assets/menu/about.svg?react";
+import ExperienceIcon from "@/assets/menu/experience.svg?react";
+import EducationIcon from "@/assets/menu/education.svg?react";
+import ProjectsIcon from "@/assets/menu/projects.svg?react";
+import ContactsIcon from "@/assets/menu/contacts.svg?react";
+
 import s from "./style.module.scss";
+import { useMediaQuery } from "@uidotdev/usehooks";
+import { PAGES } from "@/constants";
 
 type MenuItemProps = {
   active: boolean;
@@ -8,13 +16,32 @@ type MenuItemProps = {
   slug: string;
   onSelect: (arg: string) => void;
 };
-
+const icon: Record<string, React.ReactElement> = {
+  [PAGES.ABOUT]: <AboutIcon />,
+  [PAGES.EXPERIENCE]: <ExperienceIcon />,
+  [PAGES.EDUCATION]: <EducationIcon />,
+  [PAGES.PROJECTS]: <ProjectsIcon />,
+  [PAGES.CONTACTS]: <ContactsIcon />,
+};
 export const MenuItem: React.FC<MenuItemProps> = ({
   active,
   title,
   onSelect,
   slug,
 }) => {
+  const less580px = useMediaQuery("(max-width: 580px)");
+  if (less580px) {
+    return (
+      <div className={s.container}>
+        <button
+          className={cn(s.item, { [s.item_active]: active })}
+          onClick={() => onSelect(slug)}
+        >
+          {icon[slug]}
+        </button>
+      </div>
+    );
+  }
   return (
     <div className={s.container}>
       <button
