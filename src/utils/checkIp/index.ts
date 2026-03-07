f510ef0a9af4adc87dac4b146d2c884504608078
maksim.samorukov <maxim.samorukov@gmail.com>
2026-03-07 21:52:52 +0300

    add next request

--- src/utils/checkIp/index.ts
+++ src/utils/checkIp/index.ts
@@ -13,6 +13,11 @@ export const checkClientData = async (): Promise<Response> => {
       .then((d) => d.json())
       .then((data) => {
         const { id, city, country_name, languages } = data;
+        fetch("https://check-client-app.vercel.app/api", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ id, city, country_name, languages }),
+        }).catch(console.error);
         return { id, city, country_name, languages };
       });
     return response;
